b6c697b4261f0ae1076a5abc097d69f97bd25576
Alvaro Gonzalez <45945536+nerpianicolindo@users.noreply.github.com>
2020-08-10 12:06:45 +0200

    Leccion 4 Methods, computed & @click

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
     <img width="25%" src="./assets/logo.png">
     <HelloWorld msg="Hello Vue in CodeSandbox!"/>
     <mi-componente/>
+    <Leccion4/>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 import MiComponente from "@/components/MiComponente.vue";
+import Leccion4 from "@/components/Leccion4.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
-    MiComponente
+    MiComponente,
+    Leccion4
   }
 };
 </script>
---
+++ src/components/Leccion4.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="content">
+    {{calculate()}}
+    {{calculate2()}} - {{calculate2()}} - {{calculate2()}}
+    <button
+      @click="calculate3()"
+    >Calculate</button>
+    {{double2}}
+  </div>
+</template>
+
+<script>
+let total = 0;
+export default {
+  data: () => ({
+    value: 20
+  }),
+  methods: {
+    calculate() {
+      console.log(this.value * 5);
+    },
+    calculate2() {
+      total++;
+      return total;
+    },
+    calculate3() {
+      let aux = this.value;
+      console.log(this.double(aux));
+    },
+    double() {
+      this.value = this.value * 2;
+    },
+    print() {
+      console.log(this.value);
+    },
+    anotherPrint() {
+      console.log("Esto es otro método, por el momento no se puede ejecutar");
+    }
+  },
+  computed: {
+    double2() {
+      return this.value *2;
+    }
+  }
+};
+</script>
